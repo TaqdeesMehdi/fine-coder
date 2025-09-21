2d478575ea1a32d2ea0ea2389e7101ea6c22b46f
Taqdees Mehdi <taqdeesm99@gmail.com>
2025-09-21 11:36:18 +0500

    database/prisma added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-export default function Home() {
+export default async function Home() {
   return (
-    <div className="text-4xl font-extrabold text-blue-500">Fine Coder</div>
+    <div className="text-4xl font-extrabold text-blue-500">Fine coder</div>
   );
 }
