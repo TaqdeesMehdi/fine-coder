2f3954f4ba42363c59893bd8a6b9a5c33f41896d
hamzaah3 <ameerhamza.dev3@gmail.com>
2025-10-04 14:10:03 +0500

    added the e2b sandboxes , initialized docker images

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,9 +1,15 @@
 import { inngest } from "./client";
 import { openai, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("fine-coder-nextjs-taqdees-1122");
+      return sandbox.sandboxId;
+    });
     const codeAgent = createAgent({
       name: "summarizer",
       system:
@@ -13,7 +19,11 @@ export const helloWorld = inngest.createFunction(
     const { output } = await codeAgent.run(
       `Write the following snippet:${event.data.value}`
     );
-
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+    return { output, sandboxUrl };
   }
 );
